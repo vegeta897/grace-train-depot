8063c4b038f82d28329a9cde55e549ade62194b8
Devin Spikowski <vegeta897@gmail.com>
2023-05-11 01:28:47 -0400

    🎉 Bootstrapping

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,53 +4,11 @@
 	import './styles.css'
 </script>
 
-<div class="app">
-	<Header />
-
-	<main>
+<div class="flex h-screen items-start justify-center bg-base-200 p-4">
+	<div class="max-w-lg rounded-2xl bg-base-100 p-6">
 		<slot />
-	</main>
-
-	<footer>
-		<p>
-			visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
-		</p>
-	</footer>
+	</div>
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
 </style>
