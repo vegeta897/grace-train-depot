8587d5bcf0dd33e09f7be20f1bcfbe2ae8699043
Devin Spikowski <vegeta897@gmail.com>
2023-05-11 04:32:23 -0400

    ➕ Add nunito font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
-<script>
+<script lang="ts">
 	import '../app.postcss'
 	import Header from './Header.svelte'
+	import '@fontsource/nunito/900.css'
+	import '@fontsource/nunito/variable.css'
 	import './styles.css'
 </script>
 
-<div class="flex h-screen items-start justify-center bg-base-200 p-4">
-	<div class="max-w-lg rounded-2xl bg-base-100 p-6">
+<div class="flex h-screen flex-col items-center justify-center bg-base-100 p-4">
+	<div class="max-w-lg rounded-2xl bg-base-200 p-6 text-neutral-content">
 		<slot />
 	</div>
 </div>
