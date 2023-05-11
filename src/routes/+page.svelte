46808d121a38cbf940a6e1da7437667467918a1c
Devin Spikowski <vegeta897@gmail.com>
2023-05-11 04:33:10 -0400

    ✨ Look, a menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
-<script>
+<script lang="ts">
+	const buttons = [
+		['🚌', 'Body'],
+		['⭐', 'Decals'],
+		['🎡', 'Wheels'],
+		['🎓', 'Toppers'],
+		['✨', 'Effects'],
+	]
 </script>
 
 <svelte:head>
@@ -7,8 +14,16 @@
 </svelte:head>
 
 <section>
-	<h1 class="text-center text-2xl font-bold">Grace Train Depot</h1>
-	<p>Customize your Grace train car!</p>
+	<h1 class="nunito text-center text-2xl uppercase">Grace Train Depot</h1>
+	<p class="mb-4 mt-2">Customize your Grace train car!</p>
+	<div class="nunito flex flex-col space-y-2">
+		{#each buttons as [icon, title]}
+			<btn class="btn-block btn-lg btn justify-start gap-8 text-xl">
+				<div class="w-12 text-center text-4xl">{icon}</div>
+				{title}
+			</btn>
+		{/each}
+	</div>
 </section>
 
 <style>
