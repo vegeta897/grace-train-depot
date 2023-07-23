68f95237f58e33545a3e6dc5ea5e8ba0c4db2440
Devin Spikowski <vegeta897@gmail.com>
2023-07-23 04:05:36 -0400

    ➖ Replace nanoid with lucia util

--- src/routes/design/new/+server.ts
+++ src/routes/design/new/+server.ts
@@ -1,5 +1,5 @@
 import prisma from '$lib/server/prisma'
-import { customAlphabet } from 'nanoid'
+import { generateRandomString } from 'lucia/utils'
 import { redirect, type RequestHandler } from '@sveltejs/kit'
 import { COLORS } from 'grace-train-lib'
 
@@ -8,12 +8,10 @@ import { COLORS } from 'grace-train-lib'
 const shortIdAlphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 const shortIdLength = 6
 
-const generateShortId = customAlphabet(shortIdAlphabet, shortIdLength)
-
 export const GET = (async ({ locals }) => {
 	const session = await locals.auth.validate()
 	if (!session) throw redirect(302, '/')
-	const shortId = generateShortId()
+	const shortId = generateRandomString(shortIdLength, shortIdAlphabet)
 	await prisma.car.create({
 		data: {
 			shortId,
