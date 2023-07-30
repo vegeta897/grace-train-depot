2ead3e992faed7ca97fe9cb033d5b74710865eec
Devin Spikowski <vegeta897@gmail.com>
2023-07-30 06:45:31 -0400

    🧑‍💻 Add auth skip mode

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,15 @@
+import { dev } from '$app/environment'
+import { SKIP_AUTH } from '$env/static/private'
 import { auth } from '$lib/server/lucia'
+import prisma from '$lib/server/prisma'
 import type { Handle } from '@sveltejs/kit'
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event)
+	if (dev && SKIP_AUTH === 'true') {
+		const sessionRecord = await prisma.session.findFirstOrThrow()
+		const luciaSession = await auth.getSession(sessionRecord.id)
+		event.locals.auth.setSession(luciaSession)
+	}
 	return await resolve(event)
 }
