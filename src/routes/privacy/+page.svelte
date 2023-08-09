b41801a2c1d80690589eed72d2edb9039f9dcd25
Devin Spikowski <vegeta897@gmail.com>
2023-08-09 15:06:00 -0400

    📝 Add privacy statement

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,33 @@
+<section class="flex flex-col gap-4 lg:px-8 px-4">
+	<h2 class="text-2xl font-bold">Privacy</h2>
+	<h3 class="text-xl font-bold">Twitch Authentication</h3>
+	<p>
+		Grace Train Depot requires you to log in with your Twitch account. This is a safe
+		process known as <em>OAuth</em> that lets you verify your identity without revealing any
+		personal info (not even your email) or granting unnecessary access to your account. The
+		only information you give to our site is your Twitch username and a numeric Twitch ID,
+		which is not private information. Our site is incapable of performing any actions on behalf
+		of your Twitch account.
+	</p>
+	<h3 class="text-xl font-bold">User Sessions</h3>
+	<p>
+		This site uses cookies to track user sessions, so you don't have to log in every time
+		you visit. The cookies are just a long string of randomly generated characters. These
+		strings are only meaningful to our server, which links them to Twitch users.
+	</p>
+	<h3 class="text-xl font-bold">Your Content</h3>
+	<p>
+		The content you create while on this site is subject to being shared to other
+		visitors, as well as on any streams and stream archives that have an overlay utilizing
+		this data. In plain terms, the point of this site is to create train car designs to
+		show them in Grace Trains, and your designs may be viewed by others who visit the
+		site. Your designs may be publicly attributed (directly or indirectly) to your Twitch
+		username both on the site and in streams.
+	</p>
+	<h3 class="text-xl font-bold">Account Removal</h3>
+	<p>
+		You may delete your account at any time, which will remove all of your user
+		information and content. You may log in with Twitch again afterwards, but your train
+		car designs will be gone.
+	</p>
+</section>
