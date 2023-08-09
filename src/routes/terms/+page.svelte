adb209c5a7641a6bda88b286d891cf7a8cb0c13e
Devin Spikowski <vegeta897@gmail.com>
2023-08-09 15:25:09 -0400

    📝 Add terms of use page

---
+++ src/routes/terms/+page.svelte
@@ -0,0 +1,16 @@
+<section class="flex flex-col gap-4 lg:px-8 px-4">
+	<h2 class="text-2xl font-bold">Terms of Use</h2>
+	<h3 class="text-xl font-bold">Privacy Acknowledgment</h3>
+	<p>
+		By using this site, you agree to the use of your Twitch identity and data as outlined
+		in the <a class="link" href="privacy">Privacy</a> statement.
+	</p>
+	<h3 class="text-xl font-bold">Content & User Moderation</h3>
+	<p>
+		All content created on this site is subject to removal by the admins if it is deemed
+		abusive. Specifically, that means train car designs containing offensive imagery. Any
+		user found to be intentionally abusing the site in any way is subject to termination.
+		Your actions on this site may be used as justification for bans within the associated
+		Twitch channel.
+	</p>
+</section>
