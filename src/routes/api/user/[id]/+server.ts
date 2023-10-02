0febb86bd5cc24e8d404824b17e659b160a00c6a
Devin Spikowski <vegeta897@gmail.com>
2023-10-02 09:01:36 -0400

    👔 Update user endpoint

--- src/routes/api/user/[id]/+server.ts
+++ src/routes/api/user/[id]/+server.ts
@@ -2,6 +2,9 @@ import { DEPOT_SECRET } from '$env/static/private'
 import { error, json } from '@sveltejs/kit'
 import type { RequestHandler } from './$types'
 import prisma from '$lib/server/prisma'
+import type { CarData } from '$lib/server/schemas'
+import type { GraceTrainCar } from 'grace-train-lib/trains'
+import { transformCarFromDB } from '$lib/car'
 
 export const GET = (async ({ request, params }) => {
 	console.log('/api/car GET received!', params.id)
@@ -9,10 +12,35 @@ export const GET = (async ({ request, params }) => {
 	if (authHeader !== DEPOT_SECRET) throw error(401)
 	const userWithCars = await prisma.user.findUnique({
 		where: { twitchUserId: params.id },
-		include: { cars: { include: { decals: true } } },
+		include: { cars: { include: { decals: true, toppers: true } } },
 	})
 	if (!userWithCars) throw error(404, 'User not found')
-	// TODO: Return only essential properties
-	// Use a type defined in grace-train-lib so spice bot knows what to expect
-	return json(userWithCars)
+	return json({
+		userId: userWithCars.id,
+		cars: userWithCars.cars.map((c) => transformCarToWS(transformCarFromDB(c))),
+	})
 }) satisfies RequestHandler
+
+function transformCarToWS(car: CarData): GraceTrainCar {
+	return {
+		body: car.body,
+		bodyColor: car.bodyColor,
+		bodyPopColor: car.bodyPopColor,
+		wheelColor: car.wheels.color,
+		wheelFromCenter: car.wheels.fromCenter,
+		decals: car.decals.map((d) => ({
+			name: d.name,
+			fill: d.fill,
+			...d.transform,
+			params: d.params,
+		})),
+		toppers: car.toppers.map((t) => ({
+			name: t.name,
+			colors: t.colors,
+			position: t.position,
+			offset: t.offset,
+			scale: t.scale,
+			rotate: t.rotate,
+		})),
+	}
+}
