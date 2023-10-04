c4836ae2f60ffcfc4bd8430cc71d7327d410d95f
Devin Spikowski <vegeta897@gmail.com>
2023-10-04 00:39:20 -0400

    ✨ Add ping endpoint

---
+++ src/routes/api/ping/+server.ts
@@ -0,0 +1,10 @@
+import { DEPOT_SECRET } from '$env/static/private'
+import { error } from '@sveltejs/kit'
+import type { RequestHandler } from './$types'
+
+export const GET = (async ({ request }) => {
+	console.log('/api/ping GET received!')
+	const authHeader = request.headers.get('Authorization')
+	if (authHeader !== DEPOT_SECRET) throw error(401)
+	return new Response('pong!')
+}) satisfies RequestHandler
