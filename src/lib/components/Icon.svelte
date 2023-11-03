552bb4a77b269ffc6dda1656dc6a5225f1fbd143
Devin Spikowski <vegeta897@gmail.com>
2023-11-02 22:05:04 -0400

    🚚 Move unused svg icons to a component

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,36 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 9" class="w-full stroke-current">
+	<path
+		class="origin-center transition-transform"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="7"
+		vector-effect="non-scaling-stroke"
+		d="M2,2 L7,7 M7,2 L2,7"
+	/>
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 9" class="w-full stroke-current">
+	<path
+		class="origin-center transition-transform"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="7"
+		vector-effect="non-scaling-stroke"
+		d="M4.5,1.5 V7.5 M1.5,4.5 H7.5"
+	/>
+</svg>
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	class="h-1/2 w-1/2 stroke-current"
+>
+	<path
+		class="origin-center transition-transform"
+		style:transform="scale(1)"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="6"
+		vector-effect="non-scaling-stroke"
+		d="M21,3 L3,21 M21,3 h-10 M21,3 v10"
+	/>
+</svg>
