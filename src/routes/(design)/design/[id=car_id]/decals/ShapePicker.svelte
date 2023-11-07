5f4f366b90f3ce24c1cbe6af14256b7efc243a29
Devin Spikowski <vegeta897@gmail.com>
2023-11-07 07:56:01 -0500

    ✨ Add tabs to shape picker

--- src/routes/(design)/design/[id=car_id]/decals/ShapePicker.svelte
+++ src/routes/(design)/design/[id=car_id]/decals/ShapePicker.svelte
@@ -19,22 +19,43 @@
 	export let fillOverride: string | undefined = undefined
 	export let onClick: (decalProps: DecalChoice) => void
 
-	const decalChoices: DecalChoice[] = [
-		{ name: 'star', fill: COLORS.POP[4] },
-		{ name: 'heart', fill: COLORS.POP[1] },
-		{ name: 'circle', fill: COLORS.POP[6] },
-		...PRIDE_FLAGS.map(
-			(flag) => ({ name: 'flag', defaultParams: { flag } }) as DecalChoice
-		),
+	let selectedTabIndex = 0
+
+	const tabs: DecalChoice[][] = [
+		[
+			{ name: 'star', fill: COLORS.POP[4] },
+			{ name: 'heart', fill: COLORS.POP[1] },
+			{ name: 'circle', fill: COLORS.POP[6] },
+		],
+		PRIDE_FLAGS.map((flag) => ({ name: 'flag', defaultParams: { flag } }) as DecalChoice),
 	]
 </script>
 
-<div class="grid grid-cols-6 gap-2">
-	{#each decalChoices as { name, fill, defaultParams }}
+<!-- TODO: Move tabs inside decal box, vertical on right-hand side -->
+<div class="join mb-2">
+	{#each tabs as [{ name, fill }], t}
+		<button
+			on:click={() => (selectedTabIndex = t)}
+			class="btn join-item w-16"
+			class:btn-active={t === selectedTabIndex}
+			class:btn-primary={t === selectedTabIndex}
+		>
+			<ContainerSvg viewBox="-50 -50 100 100">
+				<Decal
+					{name}
+					fill={fillOverride || fill}
+					params={decalDefs[name].getDefaultParamsObject()}
+				/>
+			</ContainerSvg>
+		</button>
+	{/each}
+</div>
+<div class="grid grid-cols-6 gap-1 rounded-lg bg-base-100 p-2">
+	{#each tabs[selectedTabIndex] as { name, fill, defaultParams }}
 		{@const params = { ...decalDefs[name].getDefaultParamsObject(), ...defaultParams }}
 		<button
 			on:click={() => onClick({ name, fill, defaultParams })}
-			class="btn-hover-grow btn h-auto w-full touch-manipulation p-2"
+			class="btn-hover-grow btn btn-ghost h-auto w-full touch-manipulation p-1"
 		>
 			<ContainerSvg viewBox="-50 -50 100 100">
 				<Decal {name} fill={fillOverride || fill} {params} />
