620b60fb9446c35ad8e607d4fcac20766e150a07
Devin Spikowski <vegeta897@gmail.com>
2023-11-18 07:51:24 -0500

    ⚡️ Optimize bounding box math

--- src/routes/(design)/design/[id=car_id]/decals/BoundingBox.svelte
+++ src/routes/(design)/design/[id=car_id]/decals/BoundingBox.svelte
@@ -8,39 +8,41 @@
 	export let strokeWidthScale = 1
 	export let fullHitbox: boolean = false
 
-	const cornerAngleTrim = 25 // 25 deg trim = 40 deg sweep
-
 	const degToRad = (deg: number) => deg * (Math.PI / 180)
+
+	const cornerAngleTrim = 25 // 25 deg trim = 40 deg sweep
+	const cornerCosUnit = 1 - Math.cos(degToRad(cornerAngleTrim))
+	const cornerSinUnit = Math.sin(degToRad(cornerAngleTrim))
+	const idealDashUnit = degToRad(90 - cornerAngleTrim * 2)
 
 	$: pad = 10 / scale
 	$: pWidth = width + pad
 	$: pHeight = height + pad
 	$: rect = { x: -pWidth / 2, y: -pHeight / 2, width: pWidth, height: pHeight }
 	$: cRadius = (25 + pad) / 2 // Should complement decals with 25 stroke-width corners
-	$: ccos = cRadius * (1 - Math.cos(degToRad(cornerAngleTrim)))
-	$: csin = cRadius * Math.sin(degToRad(cornerAngleTrim))
-	$: cs = cRadius - ccos - csin
-	$: availableWidth = pWidth - (ccos + cs) * 2
-	$: availableHeight = pHeight - (ccos + cs) * 2
-	$: idealUnit = degToRad(90 - cornerAngleTrim * 2) * cRadius // Matches corner length
-	$: widthUnits = calcUnits(idealUnit, availableWidth)
-	$: dashedWidth = (widthUnits[1] - 2) * widthUnits[0]
+	$: ccos = cRadius * cornerCosUnit
+	$: cSize = cRadius - ccos - cRadius * cornerSinUnit
+	$: availableWidth = pWidth - (ccos + cSize) * 2
+	$: availableHeight = pHeight - (ccos + cSize) * 2
+	$: idealDash = idealDashUnit * cRadius // Matches corner length
+	$: widthDashes = calcDashes(idealDash, availableWidth)
+	$: dashedWidth = (widthDashes[1] - 2) * widthDashes[0]
 	$: widthSpacer = (availableWidth - dashedWidth) / 2
-	$: heightUnits = calcUnits(idealUnit, availableHeight)
-	$: dashedHeight = (heightUnits[1] - 2) * heightUnits[0]
+	$: heightDashes = calcDashes(idealDash, availableHeight)
+	$: dashedHeight = (heightDashes[1] - 2) * heightDashes[0]
 	$: heightSpacer = (availableHeight - dashedHeight) / 2
 
-	function calcUnits(ideal: number, available: number) {
+	function calcDashes(ideal: number, available: number) {
 		if (available <= 0) return [1, 1]
 		const min = ideal / 2
-		let units = 3 // 1 dash surrounded by spaces
-		let best = [available / 3, units]
-		while (units < Math.ceil(available / min)) {
-			const size = available / units
+		let dashes = 3 // 1 dash surrounded by spaces
+		let best = [available / 3, dashes]
+		while (dashes < Math.ceil(available / min)) {
+			const size = available / dashes
 			if (Math.abs(ideal - size) < Math.abs(ideal - best[0])) {
-				best = [size, units]
+				best = [size, dashes]
 			}
-			units += 2 // Every dash adds 2 units
+			dashes += 2 // Every dash has a gap of equal size
 		}
 		return best
 	}
@@ -55,9 +57,9 @@
 	$: cornersPath = cornerPathNodes
 		.map(
 			([mx, my, ax, ay], c) =>
-				`m${availableWidth * mx - (c === 0 ? cs : 0)},${
+				`m${availableWidth * mx - (c === 0 ? cSize : 0)},${
 					availableHeight * my
-				} a${cRadius} ${cRadius} 0 0 1 ${cs * ax} ${cs * ay}`
+				} a${cRadius} ${cRadius} 0 0 1 ${cSize * ax} ${cSize * ay}`
 		)
 		.join(' ')
 </script>
@@ -77,15 +79,15 @@
 		{/if}
 		<!-- Horizontal dashes -->
 		<path
-			stroke-dasharray={widthUnits[0]}
-			d="M{-pWidth / 2 + cs + ccos},{-pHeight /
+			stroke-dasharray={widthDashes[0]}
+			d="M{-pWidth / 2 + cSize + ccos},{-pHeight /
 				2} m{widthSpacer},0 h{dashedWidth} m0,{pHeight} h{-dashedWidth}"
 		/>
 		<!-- Vertical dashes -->
 		<path
-			stroke-dasharray={heightUnits[0]}
+			stroke-dasharray={heightDashes[0]}
 			d="M{-pWidth / 2},{-pHeight / 2 +
-				cs +
+				cSize +
 				ccos} m0,{heightSpacer} v{dashedHeight} m{pWidth},0 v{-dashedHeight}"
 		/>
 	</g>
