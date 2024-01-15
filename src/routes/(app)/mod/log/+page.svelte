9c6facec7bc980e70520a2934cc72e123185fa4d
Devin Spikowski <vegeta897@gmail.com>
2024-01-15 05:48:43 -0500

    ✨ Handle display of audit log actions

--- src/routes/(app)/mod/log/+page.svelte
+++ src/routes/(app)/mod/log/+page.svelte
@@ -3,7 +3,7 @@
 
 	export let data: PageData
 
-	async function getMoreEntries() {
+	async function loadMoreEntries() {
 		const response = await fetch(`/mod/log?before=${data.lastId}`)
 		const moreLogEntries = (await response.json()) as PageData['logEntries']
 		if (moreLogEntries.length === 0) {
@@ -22,10 +22,10 @@
 	<a href="/mod" class="btn">Back to Mod View</a>
 </div>
 <section class="p-4">
-	<div class="rounded-box flex flex-col gap-4 bg-neutral p-4">
+	<div class="rounded-box flex flex-col gap-4 bg-neutral p-4 lg:px-8 lg:py-6">
 		<h2 class="text-2xl font-black uppercase tracking-wide">📄 Audit log</h2>
-		<table class="table table-lg">
-			<thead class="text-base">
+		<table class="table table-zebra table-xs sm:table-md lg:table-lg">
+			<thead class="sm:text-base">
 				<tr>
 					<th>date</th>
 					<th>time</th>
@@ -39,24 +39,50 @@
 					<tr>
 						<td>{addedAt.toLocaleDateString()}</td>
 						<td>{addedAt.toLocaleTimeString()}</td>
-						<td>{logEntry.modUser?.twitchDisplayName}</td>
 						<td>
-							{logEntry.action}
-							{logEntry.data}
-							{logEntry.onUser?.twitchDisplayName}
+							{#if logEntry.modUser}
+								<a class="link" href="/mod/users/{logEntry.modUser.id}">
+									{logEntry.modUser.twitchDisplayName}
+								</a>
+							{:else}
+								<em class="text-base-content/50">deleted user</em>
+							{/if}
+						</td>
+						<td>
+							{#if logEntry.action === 'changeUserLevel'}
+								changed
+								{#if logEntry.onUser}
+									user
+									<a class="link" href="/mod/users/{logEntry.onUser.id}">
+										{logEntry.onUser.twitchDisplayName}
+									</a>
+								{:else}
+									<em class="text-base-content/50">(deleted user)</em>
+								{/if}
+								to
+								<strong
+									class:text-success={logEntry.data === 'trusted'}
+									class:text-warning={logEntry.data === 'hidden'}
+									class:text-error={logEntry.data === 'banned'}
+								>
+									{logEntry.data}
+								</strong>
+							{:else}
+								deleted {logEntry.data}
+							{/if}
 						</td>
 					</tr>
 				{/each}
 			</tbody>
 		</table>
-		{#if data.lastId}
-			<div>
-				<button class="btn btn-outline" on:click={() => getMoreEntries()}>
+		<div class="flex justify-center">
+			{#if data.lastId}
+				<button class="btn btn-outline" on:click={() => loadMoreEntries()}>
 					load more
 				</button>
-			</div>
-		{:else}
-			<p class="p-1 text-lg italic text-base-content/50">end of log</p>
-		{/if}
+			{:else}
+				<p class="p-1 text-lg italic text-base-content/50">end of log</p>
+			{/if}
+		</div>
 	</div>
 </section>
